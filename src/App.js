06932c9d08be7515c4ed7efd6d53e33c5940f8fc
cjachekang <cjache@berkeley.edu>
2020-05-22 19:39:10 -0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    /* States for the navbar */
+    this.state = {
+      title: 'Cjache Kang',
+      headerLinks: [
+        { title: 'Home', path: '/',
+          title: 'About', path: '/about',
+          title: 'Random', path: '/random' }
+      ],
+      home: {
+        title: 'title',
+        subTitle: 'subtitle',
+        bodyText: 'blah blah blah'
+      },
+      about: {
+        title: 'About Me',
+      },
+      random: {
+        title: "Some Random Stuff I'm Working on",
+        subTitle: 'subtitle',
+        bodyText: 'blah blah blah'
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        
+        <Container className = "p-0" fluid = { true }>
+          <Navbar className = "border-bottom" bg = "transparent" expand = "lg">
+            <Navbar.Brand >Cjache's Website! (Work in Progress)</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-control="navbar-toggle"/>
+            <Navbar.Collapse id = "navbar-toggle">
+              <Nav className = "ml-auto">
+                <Link className = "nav-link" to="/">Home</Link>
+                <Link className = "nav-link" to="/about">About</Link>
+                <Link className = "nav-link" to="/random">Random</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
